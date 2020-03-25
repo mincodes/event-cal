0463142ce988c6a280705124da7172e5105c6d08
Glitch (hello-webpage) <none>
2020-03-25 03:20:21 +0000

    😂🐴 Checkpoint ./index.html:13518360/390 ./submit.js:2117330/206 ./.glitch-assets:13518354/502

--- submit.js
+++ submit.js
@@ -3,3 +3,23 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+
+const ACCESS_MAP = {
+  "amlkm2lkm": {
+    name: {
+      
+    }
+  }
+}
+
+const formFields = [
+  "name"
+]
+
+function checkAccessCode() {
+  
+}
+
+
